4a8246c198d6d51e1f568fca44fd6cf0d2123e2d
Jonathan Tellez <jonathanbtellez@gmail.com>
2022-08-02 14:19:31 -0500

    formulario parte 1

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,29 @@
 
-import { valida } from "./validacion.js";
-
+import { singInScreen, validation, singValidationForm } from "./validacion.js";
+/*usa la funcion valida que comprueba el formulario contacto*/
 const formContact = document.querySelector("[data-form-contact]");
 
 formContact.addEventListener("submit",(e)=>{
     e.preventDefault();
     const infoNombre = formContact.querySelector("[data-contact-nombre]").value;
     const infoMensaje = formContact.querySelector("[data-contact-mensaje]").value;
-    valida(infoNombre,infoMensaje);
+    validation(infoNombre,infoMensaje);
 });
 
+//Permite al usuario accceder a la pantalla de registro
+const btnSingIn = document.querySelector("[data-btn-singIn]");
+
+btnSingIn.addEventListener("click",()=>{
+    const sectionLogin = document.querySelector("[data-section-login]");
+    const sectionSingin = document.querySelector("[data-section-singin]");
+    singInScreen(sectionLogin,sectionSingin);
+});
+
+const singInForm = document.querySelector("[data-singin-form]");
+
+singInForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const singInEmail = document.querySelector("[data-singin-email]").value;
+    const singInPassword1 = document.querySelector("[data-singin-password1]").value;
+    singValidationForm(singInEmail,singInPassword1);
+})
--- js/validacion.js
+++ js/validacion.js
@@ -1,4 +1,8 @@
-export const valida = (infoNombre, infoMensaje) => { 
+/*Esta funcion recibe los datos del formulario contacto 
+valida que los datos cumplan con los parametros establecidos
+si los datos son correcto reenvia al usuario ala pagina registro completo*/
+
+export const validation = (infoNombre, infoMensaje) => {
     let contenido = "";
     const mensajeError = document.querySelector("#formulario__contacto__error");
     mensajeError.innerHTML = contenido;
@@ -24,10 +28,86 @@ export const valida = (infoNombre, infoMensaje) => {
             mensajeValido = true;
         }
     }
-    
-    if(nombreValido && mensajeValido){
-        const enviado = window.location.href ="/screens/formulario-enviado.html";
+
+    if (nombreValido && mensajeValido) {
+        const enviado = window.location.href = "/screens/formulario-enviado.html";
         infoNombre = "";
         infoMensaje = "";
-    }    
+    }
+}
+
+/*Esta funcion captura los elementos de ingreso y 
+registro si el usuario no esta registrado al pulsar el 
+boton mostrara el formulario registro*/
+
+export const singInScreen = (sectionLogin, sectionSingin) => {
+    sectionLogin.classList.add("invisible");
+    sectionSingin.classList.remove("invisible");
+}
+
+/*Esta funcion recibe los datos del formulario registro 
+valida que los datos cumplan con los parametros establecidos
+si los datos son correctos le muestra al usuario el panel de acceso*/
+
+export const singValidationForm = (singInEmail, singInPassword1) => {
+    testEmail(singInEmail);
+    testPassword1(singInPassword1);
+    //testPassword2(singInPassword2);
+    //createUser(emailValide,password1Valide)
+};
+
+
+const testEmail = (singInEmail) => {
+    const regex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+    const singInEmailFail = document.querySelector("[data-singin-email-error]");
+    let contenido = "";
+    if (regex.test(singInEmail)) {
+        singInEmailFail.classList.add("invisible");
+        const emailValide = singInEmail.toLowerCase();
+        regex.test(singInEmail);
+        return emailValide;
+    } else {
+        singInEmailFail.classList.remove("invisible");
+        contenido = `<i class="fa-solid fa-triangle-exclamation"></i> El correo ${singInEmail} no es valido, debe tener el siguiente formato nombre@correo.com.`;
+        singInEmailFail.innerHTML = contenido;
+    }
+}
+
+const testPassword1 = (singInPassword1,singInPassword2) =>{
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+    const singInPasswordFail = document.querySelector("[data-singin-password1-error]");
+    let contenido = "";
+    if (regex.test(singInPassword1)) {
+        singInPasswordFail.classList.add("invisible");
+        const password1Valide = singInPassword1;
+        console.log(password1Valide);
+        regex.test(singInPassword1);
+        const singInPassword2 = document.querySelector("[data-singin-password2]").value;
+        testPassword2(password1Valide,singInPassword2)
+        return password1Valide;
+    } else {
+        singInPasswordFail.classList.remove("invisible");
+        contenido = `<i class="fa-solid fa-triangle-exclamation"></i> La contraseña no es valido, debe tener el siguiente formato:<br>
+        <i class="fa-solid fa-triangle-exclamation"></i>Al menos 8 caracteres<br>
+        <i class="fa-solid fa-triangle-exclamation"></i>Una letra mayuscula y una minuscula<br>
+        <i class="fa-solid fa-triangle-exclamation"></i>Puede contener caracteres especiales`
+        singInPasswordFail.innerHTML = contenido;
+    }
+}
+
+const testPassword2 = (password1Valide,singInPassword2) =>{
+    const singInPasswordFail = document.querySelector("[data-singin-password2-error]");
+    let contenido = "";
+    console.log(password1Valide)
+    console.log(singInPassword2)
+    if (password1Valide == singInPassword2) {
+        singInPasswordFail.classList.add("invisible");
+        const passwordUser = singInPassword2;
+        console.log(singInPassword2);
+        return passwordUser;
+    } else {
+        singInPasswordFail.classList.remove("invisible");
+        contenido = `<i class="fa-solid fa-triangle-exclamation"></i> La contraseña no es valida, debe ser igual a la contraseña anterior`
+        singInPasswordFail.innerHTML = contenido;
+    }
 }
