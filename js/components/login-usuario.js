5d70058eec0e459920c3ee4828e8f65487c92646
Jonathan Tellez <jonathanbtellez@gmail.com>
2022-08-05 15:44:05 -0500

    formulario parte 1

--- js/components/login-usuario.js
+++ js/components/login-usuario.js
@@ -1,3 +1,4 @@
+
 export const saveUser = (user) => {
     const users = JSON.parse(localStorage.getItem("user")) || [];
     users.push(user);
@@ -15,7 +16,7 @@ export const validateUser = (loginEmail, loginPassword) => {
             user.estado = 1; //1 estado activo
             userValide = user;
             console.log(indice)
-            users.splice(indice,1,userValide);
+            users.splice(indice, 1, userValide);
             localStorage.setItem("user", JSON.stringify(users));
         } else {
             console.log("No existe el usuario");
@@ -28,7 +29,7 @@ export const validateUser = (loginEmail, loginPassword) => {
         loginFail.innerHTML = contenido
         return false
     } else if (userValide == undefined) {
-        const contenido = `<i class="fa-solid fa-triangle-exclamation"></i> Verifique los datos ingresados.`
+        const contenido = `<i class="fa-solid fa-triangle-exclamation"></i> Verifique los datos ingresados o registrese.`
         loginFail.classList.remove("invisible");
         loginFail.innerHTML = contenido
         return false
@@ -40,20 +41,56 @@ export const validateUser = (loginEmail, loginPassword) => {
 }
 
 
-export const userView = (users) =>{
+export const userViewProduct = (users) => {
     users.forEach((user) => {
         console.log(user)
         if (user.estado == 1) {
             const btnLeave = document.querySelector("[ data-btn-leave]");
-            btnLeave.classList.remove("invisible");
-            
+
+            if (btnLeave != undefined) {
+                btnLeave.classList.remove("invisible");
+            }
+
             const btnNewProduct = document.querySelector("[data-new-product]");
-            btnNewProduct.classList.remove("invisible");
-            
-            const iconsProducts = document.querySelector("[data-icons-product]");
+            if (btnNewProduct != undefined) {
+                btnNewProduct.classList.remove("invisible");
+            }
+
+            const iconsProducts = document.querySelectorAll("[data-icons-product]");
             console.log(iconsProducts);
-            //iconsProducts.classList.add("productos__iconos");
-            //iconsProducts.classList.remove("invisible");
+            iconsProducts.forEach((icons) => {
+                icons.classList.add("productos__iconos");
+            });
+        }
+    });
+
+}
+
+export const userViewHome = (users) => {
+    users.forEach((user) => {
+        console.log(user)
+        if (user.estado == 1) {
+            const btnLog = document.querySelector("[data-header-container]");
+            if (btnLog != undefined) {
+                btnLog.classList.remove("invisible")
+                document.querySelector("[data-log-btn]").remove();
+                const contenido = `<button data-btn-leave class="header__link btn"><i class="fa-solid fa-arrow-right-from-bracket btn--rojo"></i></button>`;
+                btnLog.innerHTML += contenido;
+            }
         }
     })
+}
+
+export const closeSeccion = (users) => {
+ 
+    users.forEach(user => {
+        const indice = users.indexOf(user);
+        user.estado = 0; //1 estado activo
+  
+        const userValide = user;
+        users.splice(indice, 1, userValide);
+        localStorage.setItem("user", JSON.stringify(users));
+
+        window.location.href="../index.html";
+    });
 }
--- js/components/principal.js
+++ js/components/principal.js
@@ -1,51 +1,66 @@
-import { saveUser, userView, validateUser } from "./login-usuario.js";
+import { saveUser, userViewProduct, userViewHome, validateUser, closeSeccion } from "./login-usuario.js";
 import { validation } from "./validacion-contacto.js";
 import { singInScreen, singValidationForm } from "./validacion-registro.js";
 
+
+const users = JSON.parse(localStorage.getItem("user")) || [];
+userViewHome(users);
+userViewProduct(users);
 /*usa la funcion valida que comprueba el formulario contacto*/
 const formContact = document.querySelector("[data-form-contact]");
 formContact.addEventListener("submit", (e) => {
-    
+
     e.preventDefault();
-    
+
     const infoNombre = formContact.querySelector("[data-contact-nombre]").value;
     const infoMensaje = formContact.querySelector("[data-contact-mensaje]").value;
-    
+
     validation(infoNombre, infoMensaje);
 });
 
-const users = JSON.parse(localStorage.getItem("user")) || [];
-userView(users);
+
 
 //Permite al usuario accceder a la pantalla de registro
 const btnSingIn = document.querySelector("[data-btn-singIn]");
-btnSingIn.addEventListener("click", () => {
-    const sectionLogin = document.querySelector("[data-section-login]");
-    const sectionSingin = document.querySelector("[data-section-singin]");
+if (btnSingIn != undefined) {
+    btnSingIn.addEventListener("click", () => {
+        const sectionLogin = document.querySelector("[data-section-login]");
+        const sectionSingin = document.querySelector("[data-section-singin]");
 
-    singInScreen(sectionLogin, sectionSingin);
-});
+        singInScreen(sectionLogin, sectionSingin);
+    });
+}
 
 //Valida los datos del registro del usuario para crear su User
 const singInForm = document.querySelector("[data-singin-form]");
-singInForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    const singInEmail = document.querySelector("[data-singin-email]").value;
-    const singInPassword1 = document.querySelector("[data-singin-password1]").value;
-    const singInPassword2 = document.querySelector("[data-singin-password2]").value;
+if (singInForm != undefined) {
+    singInForm.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    const user = singValidationForm(singInEmail, singInPassword1, singInPassword2);
-    saveUser(user);
-});
+        const singInEmail = document.querySelector("[data-singin-email]").value;
+        const singInPassword1 = document.querySelector("[data-singin-password1]").value;
+        const singInPassword2 = document.querySelector("[data-singin-password2]").value;
+
+        const user = singValidationForm(singInEmail, singInPassword1, singInPassword2);
+        saveUser(user);
+    });
+}
 
 //validando que el usuario ingresado se encuentre registrado
 const logInForm = document.querySelector("[data-form-login]");
-logInForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const loginEmail = document.querySelector("[data-login-email]").value;
-    const loginPassword =document.querySelector("[data-login-password]").value;
-    validateUser(loginEmail,loginPassword);
-})
+if (logInForm != undefined) {
+    logInForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const loginEmail = document.querySelector("[data-login-email]").value;
+        const loginPassword = document.querySelector("[data-login-password]").value;
+        validateUser(loginEmail, loginPassword);
+    })
+}
 
-
+const btnLeave = document.querySelector("[data-btn-leave]");
+if(btnLeave != undefined){
+    btnLeave.addEventListener("click",()=>{
+        //const users = JSON.parse(localStorage.getItem("user")) || [];
+        closeSeccion(users)
+    })
+}
